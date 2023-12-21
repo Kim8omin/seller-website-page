c126d7233cf8a4c0865b34c202e56eb935ca659f
Bomin Kim <viaemma89@gmail.com>
2023-12-21 12:15:18 +0900

    carasel

--- xion/src/assets/carasel_imgData.js
+++ xion/src/assets/carasel_imgData.js
@@ -2,31 +2,31 @@ const imageData = [
     {
       label: "Image 1",
       alt: "image1",
-      url: "/assets/fortfolio/p1.png",
+      url: "/fortImg/fortfolio/p1.png"
     },
   
     {
       label: "Image 2",
       alt: "image2",
-      url: "https://picsum.photos/200/300",
+      url: "/fortImg/fortfolio/p2.png"
     },
   
     {
       label: "Image 3",
       alt: "image3",
-      url: "https://picsum.photos/200/300",
+      url: "/fortImg/fortfolio/p3.png"
     },
   
     {
       label: "Image 4",
       alt: "image4",
-      url: "https://picsum.photos/200/300",
+      url: "/fortImg/fortfolio/p4.png"
     },
   
     {
       label: "Image 5",
       alt: "image5",
-      url: "https://picsum.photos/200/300",
+      url: "/fortImg/fortfolio/p5.png"
     },
   ];
   
--- xion/src/components/main/fortpolio.jsx
+++ xion/src/components/main/fortpolio.jsx
@@ -1,15 +1,36 @@
-import { React} from "react";
+import { React, useState, useEffect } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import imageData from "../../assets/carasel_imgData";
+
+const renderSlides = imageData.map(image => (
+  <div key={image.alt}>
+    <img src={image.url} alt={image.alt} />
+</div>
+));
 
 
 const Fortpolio = () => {
 
+  const [currentIndex, setCurrentIndex] = useState();
+  function handleChange(index) {
+    setCurrentIndex(index);
+  }
+
   return (
-    <>
-    <h2>ha ha</h2>
-    </>
-  )
+    <div className="flex justify-center items-center py-5 px-3">
+        <Carousel
+          showArrows={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          selectedItem={imageData[currentIndex]}
+          onChange={handleChange}
+          className="w-[400px] lg:hidden">
+          {renderSlides}
+        </Carousel>
+    </div>
+  );
+};
 
-}
-
-  
 export default Fortpolio;
