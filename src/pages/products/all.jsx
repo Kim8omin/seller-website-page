c81676a23ee354e17b57af4a260e6646893c9f11
Bomin Kim <viaemma89@gmail.com>
2024-04-11 18:39:02 +0900

    menu bar css fixed

--- src/pages/products/all.jsx
+++ src/pages/products/all.jsx
@@ -101,8 +101,13 @@ const CategoryButtons = styled.div`
     &:hover {
       color: grey;
     }
-    @media (max-width: 500px) {
-      width: 50%;
+  }
+
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+    align-items: center;
+    button {
+      margin-bottom: 10px;
     }
   }
 `;
